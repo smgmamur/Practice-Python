109f508d05a31db331349a3c1021d250d308b8ef
smgmamur <32685514+smgmamur@users.noreply.github.com>
2017-10-11 11:50:52 -0400

    Update 1-1.py

--- 1-1.py
+++ 1-1.py
@@ -6,9 +6,15 @@ Created on Jul 19, 2017
 name = input("Enter name:")
 age = int(input("Now enter age:"))
 Cent_Year = (2117-age)
+i=0
 
 print("Name:", name)
 print("Age:", age)
-print("Year you will turn 100:", Cent_Year)
 
+numcopy = int(input("Enter how many times you want to watch the answer:"))
+
+while i<numcopy:
+    print("Year you will turn 100: \n", Cent_Year)
+    i=i+1
+  
  
