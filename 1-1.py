8ebb02ba3be0dd02d22ded5280d5218b33cb357d
smgmamur <32685514+smgmamur@users.noreply.github.com>
2017-10-11 13:58:58 -0400

    Update 1-1.py

--- 1-1.py
+++ 1-1.py
@@ -3,18 +3,31 @@ Created on Jul 19, 2017
 
 @author: mhossain
 '''
+
+import datetime
+now = datetime.datetime.now()
+
 name = input("Enter name:")
-age = int(input("Now enter age:"))
-Cent_Year = (2117-age)
+ageY = int(input("Now enter age:\n Years:"))
+ageM = int(input("\nMonths:"))
+
+NowY = now.year
+NowM = now.month
+
+Birth_Year = int(((NowY*12+NowM)-(ageY*12+ageM))/12)
+Cent_Year = Birth_Year + 100
+
 i=0
 
 print("Name:", name)
-print("Age:", age)
+print("Age: ", ageY, "Years ", ageM, "Months")
 
 numcopy = int(input("Enter how many times you want to watch the answer:"))
 
 while i<numcopy:
     print("Year you will turn 100: \n", Cent_Year)
     i=i+1
+    
+    
   
  
