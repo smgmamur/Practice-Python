fc09612e8f027b86568df0404eb01d6ad7bd7334
smgmamur <32685514+smgmamur@users.noreply.github.com>
2017-10-13 10:15:57 -0400

    Create 1-7.py

---
+++ 1-7.py
@@ -0,0 +1,10 @@
+'''
+Created on Oct 13, 2017
+
+@author: mhossain
+'''
+given_list = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+even_nums_list = [num for num in given_list if num%2==0]
+
+print(even_nums_list)
